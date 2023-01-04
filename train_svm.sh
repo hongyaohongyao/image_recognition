69ce4c20205373c3cbb3e3cc2ac5a60f9cf628de
hongyaohongyao <1207105685@qq.com>
2023-01-04 15:41:34 +0000

    train svm C=50

--- train_svm.sh
+++ train_svm.sh
@@ -1,10 +1,15 @@
-python train_svm.py --name mnist_rbf -k=rbf -C=1
-python train_svm.py --name mnist_linear -k=linear -C=1
-python train_svm.py --ds FashionMNIST --name fmnist_rbf -k=rbf -C=1
-python train_svm.py --ds FashionMNIST --name fmnist_linear -k=linear -C=1
-python train_svm.py --ds CIFAR10 --name cifar10_rbf -k=rbf -C=1
-python train_svm.py --ds CIFAR10 --name cifar10_linear -k=linear -C=1
+python train_svm.py --name mnist_rbf_c1 -k=rbf -C=1
+python train_svm.py --name mnist_linear_c1 -k=linear -C=1
+python train_svm.py --ds FashionMNIST --name fmnist_rbf_c1 -k=rbf -C=1
+python train_svm.py --ds FashionMNIST --name fmnist_linear_c1 -k=linear -C=1
+python train_svm.py --ds CIFAR10 --name cifar10_rbf_c1 -k=rbf -C=1
+python train_svm.py --ds CIFAR10 --name cifar10_linear_c1 -k=linear -C=1
 python train_svm.py --name mnist_rbf_c5 -k=rbf -C=5
 python train_svm.py --name mnist_rbf_c10 -k=rbf -C=10
 python train_svm.py --name mnist_rbf_c50 -k=rbf -C=50
 python train_svm.py --name mnist_rbf_c100 -k=rbf -C=100
+python train_svm.py --name mnist_linear_c50 -k=linear -C=50
+python train_svm.py --ds FashionMNIST --name fmnist_rbf_c50 -k=rbf -C=50
+python train_svm.py --ds FashionMNIST --name fmnist_linear_c50 -k=linear -C=50
+python train_svm.py --ds CIFAR10 --name cifar10_rbf_c50 -k=rbf -C=50
+python train_svm.py --ds CIFAR10 --name cifar10_linear_c50 -k=linear -C=50
---
+++ train_svm2.sh
@@ -0,0 +1,5 @@
+python train_svm.py --name mnist_linear_c50 -k=linear -C=50
+python train_svm.py --ds FashionMNIST --name fmnist_rbf_c50 -k=rbf -C=50
+python train_svm.py --ds FashionMNIST --name fmnist_linear_c50 -k=linear -C=50
+python train_svm.py --ds CIFAR10 --name cifar10_rbf_c50 -k=rbf -C=50
+python train_svm.py --ds CIFAR10 --name cifar10_linear_c50 -k=linear -C=50
